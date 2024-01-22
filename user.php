4c19a722ed8cd96dfdf141896444927dde7f6d4a
Arnav <arbindkumar42@gmail.com>
2024-01-22 22:13:25 +0530

    added logic

--- user.php
+++ user.php
@@ -1,5 +1,13 @@
 <?php
 
 function printFabonnaci(int $num) {
-    
+    if ($num <= 2) {
+        return 2;
+    }
+
+    echo printFabonnaci($num-2) + printFabonnaci($num -1);
 }
+
+echo printFabonnaci(5);
+
+// 0 1 1 2 3 5 
